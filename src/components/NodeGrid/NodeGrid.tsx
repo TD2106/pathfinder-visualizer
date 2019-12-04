c0027aaf9ee3e836461a6b81781f6cffce7fea09
Dan Tran <ldt999uow@gmail.com>
2019-12-05 04:30:40 +1100

    Fix Node grid style

--- src/components/NodeGrid/NodeGrid.tsx
+++ src/components/NodeGrid/NodeGrid.tsx
@@ -34,7 +34,7 @@ export const NodeGrid = (props: Props): JSX.Element => {
                     <Node
                         key={nodeKey}
                         nodeData={nodeData}
-                        isDisplayWeight={props.algorithmType === DIJKSTRA}
+                        isDisplayWeight={props.algorithmType === DIJKSTRA && !nodeData.isWall}
                         isStart={isSamePosition(
                             nodeData,
                             props.gridMarkedPosition.start,
@@ -52,7 +52,7 @@ export const NodeGrid = (props: Props): JSX.Element => {
         <div
             className={NODE_GRID_CLASS}
             style={{
-                gridTemplateColumns: `repeat(${props.gridConfig.cols}, 25px)`,
+                gridTemplateColumns: `repeat(${props.gridConfig.cols}, 26px)`,
             }}
         >
             {getNodeElements()}
